12051431f18cb8b8d0c16f68f171bfd826313eda
onyasumi <boku@kaneki.dev>
2023-11-17 17:41:53 -0500

    check arg count

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+
+    let args: Box<[String]> = std::env::args().collect();
+
+    assert!(args.len() > 3);
+
+    let source_dir: &Path = Path::new(&args[1]);
+    let dest_dir: &Path = Path::new(&args[2]);
+
+    assert!(!source_dir.exists());
+    assert!(!dest_dir.exists());
+
 }
